8236906f799dbf3cf81ee85bcb64d3e6eb3331dd
XLW <1205649229@qq.com>
2023-05-29 23:08:27 +0800

    新增课程service接口

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -2,8 +2,11 @@ package com.xuecheng.content.service;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @Title: CourseBaseInfoService
@@ -13,15 +16,22 @@ import com.xuecheng.content.model.po.CourseBase;
  * @description: 课程信息管理接口
  */
 public interface CourseBaseInfoService {
-    /*
-    *
-    * 课程分页查询
-    *
-    * pageParams 分页查询参数
-    * queryCourseParamsDto 查询条件
-    *   renturn 返回结果
-    * */
-    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+    /**
+     *
+     * @param pageParams 分页查询参数
+     * @param queryCourseParamsDto 查询条件
+     * @return 返回结果
+     */
+    public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+
+
+    /**
+     *
+     * @param companyId 机构ID
+     * @param addCourseDto 课程信息
+     * @return 课程详细信息
+     */
+    public CourseBaseInfoDto createCourseBase(Long companyId,AddCourseDto addCourseDto);
 
 
 }
