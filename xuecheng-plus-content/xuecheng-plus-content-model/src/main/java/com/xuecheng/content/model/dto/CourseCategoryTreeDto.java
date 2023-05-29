3779f9b850a5fc20c5500f5fd25aea085548cb0c
XLW <1205649229@qq.com>
2023-05-29 17:00:28 +0800

    定义一个DTO类表示分类信息的模型类

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/CourseCategoryTreeDto.java
@@ -0,0 +1,24 @@
+package com.xuecheng.content.model.dto;
+
+import com.xuecheng.content.model.po.CourseCategory;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Title: CourseCategoryTreeDto
+ * @Author XLW
+ * @Package com.xuecheng.content.model.dto
+ * @Date 2023/5/29 16:58
+ * @description: 课程分类树型节点Dto
+ */
+@Data
+@Slf4j
+public class CourseCategoryTreeDto extends CourseCategory implements Serializable {
+
+    List<CourseCategory> childrenTreeNodes;
+
+
+}
