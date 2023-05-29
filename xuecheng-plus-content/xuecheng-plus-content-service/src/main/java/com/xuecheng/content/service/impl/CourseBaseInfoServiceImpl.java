f879dfbbf11c0fcdc7a3b33f12220f656151bd02
XLW <1205649229@qq.com>
2023-05-29 23:08:34 +0800

    新增课程service接口实现类

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -5,15 +5,25 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.mapper.CourseCategoryMapper;
+import com.xuecheng.content.mapper.CourseMarketMapper;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.model.po.CourseCategory;
+import com.xuecheng.content.model.po.CourseMarket;
 import com.xuecheng.content.service.CourseBaseInfoService;
+import com.xuecheng.content.service.CourseCategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.javassist.runtime.DotClass;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -30,6 +40,13 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
 
     @Autowired
     CourseBaseMapper courseBaseMapper;
+
+
+    @Autowired
+    CourseMarketMapper courseMarketMapper;
+
+    @Autowired
+    CourseCategoryMapper courseCategoryMapper;
 
     @Override
     public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto) {
@@ -62,4 +79,155 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
 
 
     }
+
+
+    @Transactional
+    @Override
+    public CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto dto) {
+
+        //参数的合法性校验
+        if (StringUtils.isBlank(dto.getName())) {
+            throw new RuntimeException("课程名称为空");
+        }
+
+        if (StringUtils.isBlank(dto.getMt())) {
+            throw new RuntimeException("课程分类为空");
+        }
+
+        if (StringUtils.isBlank(dto.getSt())) {
+            throw new RuntimeException("课程分类为空");
+        }
+
+        if (StringUtils.isBlank(dto.getGrade())) {
+            throw new RuntimeException("课程等级为空");
+        }
+
+        if (StringUtils.isBlank(dto.getTeachmode())) {
+            throw new RuntimeException("教育模式为空");
+        }
+
+        if (StringUtils.isBlank(dto.getUsers())) {
+            throw new RuntimeException("适应人群为空");
+        }
+
+        if (StringUtils.isBlank(dto.getCharge())) {
+            throw new RuntimeException("收费规则为空");
+        }
+
+        //向课程基本信息表 course_base写入数据
+
+        //新增信息对象
+        CourseBase courseBaseNew = new CourseBase();
+
+        //将传入的页面参数放入到对象中
+        BeanUtils.copyProperties(dto,courseBaseNew);
+
+        courseBaseNew.setCompanyId(companyId);
+        courseBaseNew.setCreateDate(LocalDateTime.now());
+
+        //审核状态默认为未提交
+        courseBaseNew.setAuditStatus("202002");
+
+        //发布状态默认为未发布
+        courseBaseNew.setStatus("203001");
+
+        //插入数据库
+        int insert = courseBaseMapper.insert(courseBaseNew);
+        if (insert<=0){
+            throw new RuntimeException("添加课程失败");
+        }
+
+
+        //向课程营销信息表 course_market 写入数据
+        CourseMarket courseMarketNew = new CourseMarket();
+
+        //将页面输入的数据拷贝到courseMarketNew
+        BeanUtils.copyProperties(dto,courseMarketNew);
+
+        //课程的ID
+        Long courseId = courseBaseNew.getId();
+        //主键的课程的ID
+        courseMarketNew.setId(courseId);
+
+        //保存营销信息
+        saveCourseMarket(courseMarketNew);
+
+        //从数据库查出完整信息
+        CourseBaseInfoDto courseBaseInfo = getCourseBaseInfo(courseId);
+
+        return courseBaseInfo;
+
+    }
+
+    //查询课程信息
+    public CourseBaseInfoDto getCourseBaseInfo(long courseId){
+
+        //从课程基础信息表查询
+        CourseBase courseBase = courseBaseMapper.selectById(courseId);
+        if (courseBase == null){
+            return null;
+        }
+
+        //从课程营销表查询
+        CourseMarket courseMarket = courseMarketMapper.selectById(courseId);
+
+
+
+        //组装数据
+        CourseBaseInfoDto courseBaseInfoDto = new CourseBaseInfoDto();
+        BeanUtils.copyProperties(courseBase,courseBaseInfoDto);
+        if(courseMarket!=null){
+            BeanUtils.copyProperties(courseMarket,courseBaseInfoDto);
+        }
+
+        //通过courseCategoryMapper查询分类信息，将分类名称放在courseBaseInfoDto对象
+        //todo：课程分类的名称设置到courseBaseInfoDto
+
+        return courseBaseInfoDto;
+
+    }
+
+
+
+    //单独写一个方法保存营销信息 逻辑:存在则更新 不存在则添加
+    private  int saveCourseMarket(CourseMarket courseMarketNew){
+        //参数的合法性校验
+        String charge = courseMarketNew.getCharge();
+        if (StringUtils.isEmpty(charge)){
+            throw new RuntimeException("收费规则为空");
+        }
+
+        //如果课程收费但价格没有填写
+        if (charge.equals("201001")){
+            if (courseMarketNew.getPrice() == null || courseMarketNew.getPrice() <=0){
+                throw new RuntimeException("课程价格不能为空且必须大于0");
+            }
+
+        }
+
+
+
+        //从数据库查询营销信息 存在则更新 不存在则添加
+
+        Long id = courseMarketNew.getId();
+        CourseMarket courseMarket = courseMarketMapper.selectById(id);
+        if (courseMarket== null){
+            //插入数据库
+            int insert = courseMarketMapper.insert(courseMarketNew);
+            return insert;
+        }else {
+
+            //将courseMarketNew拷贝到courseMarket
+            BeanUtils.copyProperties(courseMarketNew,courseMarket);
+            courseMarket.setId(courseMarketNew.getId());
+            //更新
+            int i = courseMarketMapper.updateById(courseMarket);
+            return i;
+        }
+
+    }
+
+
+
+
 }
