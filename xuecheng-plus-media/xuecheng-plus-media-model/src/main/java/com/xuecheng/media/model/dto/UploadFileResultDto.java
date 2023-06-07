3b1ca9feb186a08ff431c999a510b5e5266e38f9
XLW <1205649229@qq.com>
2023-06-07 21:28:42 +0800

    设计上传文件响应类继承表对应的类（防止以后直接修改表类）

---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,13 @@
+package com.xuecheng.media.model.dto;
+
+import com.xuecheng.media.model.po.MediaFiles;
+
+/**
+ * @Title: UploadFileResultDto
+ * @Author XLW
+ * @Package com.xuecheng.media.model.dto
+ * @Date 2023/6/7 21:23
+ * @description:
+ */
+public class UploadFileResultDto extends MediaFiles {
+}
